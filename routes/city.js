1481dc2658f58549f8a38315a93c2d186b1525f0
Jerry299 <josephjeremiah564@yahoo.com>
2020-08-30 09:42:10 +0100

    moved api key

--- routes/city.js
+++ routes/city.js
@@ -14,7 +14,7 @@ router.get("/result", (req, res) => {
 
   axios
     .get(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=37068910d3019fcd4a44b0e539978cec`
     )
     .then((response) => {
       res.render("weather", { data: response.data });
@@ -24,25 +24,6 @@ router.get("/result", (req, res) => {
     });
 });
 router.get("/map-result", (req, res) => {
-  // const map = req.query.map;
-  // const longitude = req.query.long;
-  // const latitude = req.query.lat;
-  // let zoom = 6;
-
-  // const api = `https://tile.openweathermap.org/map/precipitation_new/6/6.7/6.1.png?appid=37068910d3019fcd4a44b0e539978cec`;
-  // axios
-  //   .get(
-  //     `https://tile.openweathermap.org/map/${map}/${zoom}/${longitude}/${latitude}.png?appid=${key}`
-  //   )
-  //   .then((response) => {
-  //     console.log(response.data);
-  //     res.status(200).render("mapsResult", { data: response.data });
-  //   })
-  //   .catch((err) => {
-  //     console.log("map Error", err);
-  //     res.status(404).render("cityError");
-  //   });
-
   res.render("mapsResult");
 });
 
